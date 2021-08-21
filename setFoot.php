84090a186ee0518acb0460379aacabd846e3b9ad
cokeIII <coke2484@gmail.com>
2021-08-21 13:04:02 +0700

    fix

--- setFoot.php
+++ setFoot.php
@@ -1,16 +1,16 @@
  <!-- Bootstrap core JS-->
  <!-- <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script> -->
 
- <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
  
  <!-- Core theme JS-->
  <script src="js/jquery-3.6.0.js"></script>
- <script src="vendor/datatables/jquery.dataTables.js"></script>
+
  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
  <script type="text/javascript" src="js/jquery.ui.touch-punch.min.js"></script>
  <script src="js/mainScripts.js"></script>
  <script src="js/jquery.signature.js"></script>
- <script src="http://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+ <!-- <script src="http://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script> -->
  <script src="js/jquery.redirect-spec.js"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
  <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
@@ -26,6 +26,7 @@
  <script src="vendor/jSignature-master/src/plugins/jSignature.UndoButton.js"></script>
  <script src="vendor/jSignature-master/src/plugins/signhere/jSignature.SignHere.js"></script>
  <script src="js/image-zoom.js"></script>
+ <script src="vendor/datatables/jquery.dataTables.js"></script>
  <script>
      $(function() {
          $('#imageZoom').imageZoom();
