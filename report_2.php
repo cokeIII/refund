e3dcb45eb2120bcde2d314f1e02d5b60b12a2613
cokeIII <coke2484@gmail.com>
2021-08-23 18:40:02 +0700

    re

--- report_2.php
+++ report_2.php
@@ -161,7 +161,7 @@ $row = mysqli_fetch_assoc($res);
     <div class="text-size center">สำเนาถูกต้อง</div>
     <div class="text-size width-sig">ลงชื่อ <img class="sig-size" src="uploads/signature/<?php echo $row["parent_signature"]; ?>" width="75px" height="30px"></div>
     <div class="text-size center">(<?php echo $row["recipient_fname"] . " " . $row["recipient_lname"]; ?>)</div>
-    <div class="text-size">3.เลขบัญชีธนาคารของ<?php echo $row["recipient"];?>โดยถ่ายหน้าบัญชีธนาคาร หมายเลขบัญชี <?php echo $row["recipient_bank_number"];?></div>
+    <div class="text-size">3.เลขบัญชีธนาคารของ<?php echo $row["recipient"];?>โดยถ่ายหน้าบัญชี<?php echo $row["recipient_bank"];?> หมายเลขบัญชี <?php echo $row["recipient_bank_number"];?></div>
     <div class="center"><img src="uploads/<?php echo $row["account_book_pic"]; ?>" alt="" height="135" width="275"></div>
     <div class="text-size tab">ขอรับรองว่าเป็น<?php echo $row["recipient"];?>ของ <?php echo $row["prefix_name"] . $row["stu_fname"] . " " . $row["stu_lname"]; ?></div>
     <div class="text-size">ได้รับเงินช่วยเหลือจากรัฐบาล จำนวน 2,000 บาท เรียบร้อยแล้ว</div>
