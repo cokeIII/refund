8e9cede92c0dc22af864696303b5857e811ba07c
cokeIII <coke2484@gmail.com>
2021-08-26 18:19:30 +0700

    f

--- fixDataEnroll.php
+++ fixDataEnroll.php
@@ -13,21 +13,39 @@ while ($row = mysqli_fetch_array($res)) {
     // echo  $student_id;
     //////////////////////////////////////
 
-    if($row["status"] == "พิมพ์แล้ว" || $row["status"] == "ส่งเอกสารแล้ว"){
+    // if($row["status"] == "พิมพ์แล้ว" || $row["status"] == "ส่งเอกสารแล้ว"){
+    //     $student_id = $row["student_id"];
+    //     $sqlCheck = "select status,id from enroll where status = 'เอกสารไม่ถูกต้องสมบูรณ์' and student_id = '$student_id'";
+    //     $resCheck = mysqli_query($conn,$sqlCheck);
+    //     while($rowCheck = mysqli_fetch_array($resCheck)){
+    //         $id = $rowCheck["id"];
+    //         $sqlUp = "update enroll set status = 'ยกเลิก' where id='$id'";
+    //         $resUP = mysqli_query($conn,$sqlUp);
+    //         if($resUP)
+    //             echo "ok";
+    //     }
+    // }
+    // if($row["status"] == "ส่งเอกสารแล้ว"){
+    //     $student_id = $row["student_id"];
+    //     $sqlCheck = "select status,id from enroll where status = 'ส่งเอกสารแล้ว' and student_id = '$student_id' order by id";
+    //     $resCheck = mysqli_query($conn,$sqlCheck);
+    //     $i = 0;
+    //     while($rowCheck = mysqli_fetch_array($resCheck)){
+    //         $id = $rowCheck["id"];
+    //         echo $id."<br>";
+    //         if($i > 0){
+    //             $sqlUp = "update enroll set status = 'ยกเลิก' where id='$id'";
+    //             $resUP = mysqli_query($conn,$sqlUp);
+    //             if($resUP)
+    //                 echo "ok";
+    //         }
+    //         $i++;
+    //     }
+    //     echo "---------------------<br>";
+    // }
+        if($row["status"] == "เอกสารไม่ถูกต้องสมบูรณ์"){
         $student_id = $row["student_id"];
-        $sqlCheck = "select status,id from enroll where status = 'เอกสารไม่ถูกต้องสมบูรณ์' and student_id = '$student_id'";
-        $resCheck = mysqli_query($conn,$sqlCheck);
-        while($rowCheck = mysqli_fetch_array($resCheck)){
-            $id = $rowCheck["id"];
-            $sqlUp = "update enroll set status = 'ยกเลิก' where id='$id'";
-            $resUP = mysqli_query($conn,$sqlUp);
-            if($resUP)
-                echo "ok";
-        }
-    }
-    if($row["status"] == "ส่งเอกสารแล้ว"){
-        $student_id = $row["student_id"];
-        $sqlCheck = "select status,id from enroll where status = 'ส่งเอกสารแล้ว' and student_id = '$student_id' order by id";
+        $sqlCheck = "select status,id from enroll where status = 'เอกสารไม่ถูกต้องสมบูรณ์' and student_id = '$student_id' order by id DESC";
         $resCheck = mysqli_query($conn,$sqlCheck);
         $i = 0;
         while($rowCheck = mysqli_fetch_array($resCheck)){
@@ -43,5 +61,6 @@ while ($row = mysqli_fetch_array($res)) {
         }
         echo "---------------------<br>";
     }
+
 
 }
